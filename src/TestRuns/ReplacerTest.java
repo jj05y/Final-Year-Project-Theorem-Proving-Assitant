7497d538c1d0284fa6b5ba1aa1e20ed011c5e2d0
Joe Duffin <joey.duffin@gmail.com>
2016-12-14 20:18:56 +0000

    made more clear

--- src/TestRuns/ReplacerTest.java
+++ src/TestRuns/ReplacerTest.java
@@ -18,12 +18,12 @@ public class ReplacerTest {
         long start = System.currentTimeMillis();
 
         System.out.println("Test1");
-        System.out.println("Expr is: " + Trees.absZeroequivXandY());
+        System.out.println("Expression is: " + Trees.absZeroequivXandY());
         INode selection1 = Trees.absZeroequivXandY().children()[0];
         INode rule1 =Trees.absZero();
         System.out.println("Selection is: " + selection1);
         System.out.println("Using rule: " + rule1);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection1,rule1)) System.out.println(replacement);
 
         System.out.println("Time: " + (System.currentTimeMillis()-start)+"ms");
@@ -32,12 +32,12 @@ public class ReplacerTest {
 
         start = System.currentTimeMillis();
         System.out.println("Test2");
-        System.out.println("Expr is: " + Trees.absZeroequivXandY());
+        System.out.println("Expression is: " + Trees.absZeroequivXandY());
         INode selection2 = Trees.absZeroequivXandY().children()[0];
         INode rule2 =Trees.goldenRulePQ();
         System.out.println("Selection is: " + selection2);
         System.out.println("Using rule: " + rule2);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection2,rule2)) System.out.println(replacement);
 
 
@@ -48,12 +48,12 @@ public class ReplacerTest {
 
         start = System.currentTimeMillis();
         System.out.println("Test3");
-        System.out.println("Expr is: " + Trees.weirdabsZeroequivXandY());
+        System.out.println("Expression is: " + Trees.weirdabsZeroequivXandY());
         INode selection3 = Trees.weirdabsZeroequivXandY().children()[0];
         INode rule3 =Trees.absZero();
         System.out.println("Selection is: " + selection3);
         System.out.println("Using rule: " + rule3);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection3,rule3)) System.out.println(replacement);
 
         System.out.println("Time: " + (System.currentTimeMillis()-start)+"ms");
@@ -64,12 +64,12 @@ public class ReplacerTest {
         start = System.currentTimeMillis();
 
         System.out.println("Test4");
-        System.out.println("Expr is: " + Trees.weirdBrokenabsZeroequivXandY());
+        System.out.println("Expression is: " + Trees.weirdBrokenabsZeroequivXandY());
         INode selection4 = Trees.weirdBrokenabsZeroequivXandY().children()[0];
         INode rule4 =Trees.absZero();
         System.out.println("Selection is: " + selection4);
         System.out.println("Using rule: " + rule4);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection4,rule4)) System.out.println(replacement);
 
         System.out.println("Time: " + (System.currentTimeMillis()-start)+"ms");
@@ -79,12 +79,12 @@ public class ReplacerTest {
         start = System.currentTimeMillis();
 
         System.out.println("Test5");
-        System.out.println("Expr is: " + Trees.weirdBrokenabsZeroequivXandY());
+        System.out.println("Expression is: " + Trees.weirdBrokenabsZeroequivXandY());
         INode selection5 = Trees.weirdBrokenabsZeroequivXandY().children()[0].children()[0];
         INode rule5 =Trees.absZero();
         System.out.println("Selection is: " + selection5);
         System.out.println("Using rule: " + rule5);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection5,rule5)) System.out.println(replacement);
 
         System.out.println("Time: " + (System.currentTimeMillis()-start)+"ms");
@@ -94,12 +94,12 @@ public class ReplacerTest {
         start = System.currentTimeMillis();
 
         System.out.println("Test6");
-        System.out.println("Expr is: " + Trees.weirdBrokenabsZeroequivXandY());
+        System.out.println("Expression is: " + Trees.weirdBrokenabsZeroequivXandY());
         INode selection6 = Trees.weirdBrokenabsZeroequivXandY().children()[0].children()[0];
         INode rule6 =Trees.goldenRulePQ();
         System.out.println("Selection is: " + selection6);
         System.out.println("Using rule: " + rule6);
-        System.out.println("Replacement Options: ");
+        System.out.println("New Expression Options: ");
         for (INode replacement : replacer.getReplacements(selection6,rule6)) System.out.println(replacement);
 
         System.out.println("Time: " + (System.currentTimeMillis()-start)+"ms");
